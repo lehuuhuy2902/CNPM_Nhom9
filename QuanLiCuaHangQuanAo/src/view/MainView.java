58b2c69d1063bfdc26be6c94521416e2354541a4
NguyenTranMinhHieu <74007587+NguyenTranMinhHieu@users.noreply.github.com>
2021-06-15 18:00:46 +0700

    Edit error

--- QuanLiCuaHangQuanAo/src/view/MainView.java
+++ QuanLiCuaHangQuanAo/src/view/MainView.java
@@ -6,7 +6,11 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 
