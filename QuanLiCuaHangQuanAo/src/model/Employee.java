eb1babf9e549e05e1f337ecce74f9705a78cc667
quinguyen <66554958+nguyenthiqui0354116356@users.noreply.github.com>
2021-06-20 19:31:02 +0700

    employee manager

---
+++ QuanLiCuaHangQuanAo/src/model/Employee.java
@@ -0,0 +1,85 @@
+package model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Diep.Tran
+ */
+public class Employee {
+    int id;
+    String fullname, gender, position, phoneNumber;
+    int age;
+    
+    public Employee() {
+    }
+
+    public Employee(int id, String fullname, String gender, String position, String phoneNumber, int age) {
+        this.id = id;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public Employee(String fullname, String gender, String position, String phoneNumber, int age) {
+        this.fullname = fullname;
+        this.gender = gender;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ QuanLiCuaHangQuanAo/src/model/EmployeeModify.java
@@ -0,0 +1,239 @@
+package model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * CRUD (insert, update, delete, findAll)
+ * @author Diep.Tran
+ */
+public class EmployeeModify {
+    public static List<Employee> findAll() {
+        List<Employee> employeeList = new ArrayList<>();
+        
+        Connection connection = null;
+        Statement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/qlcuahangquanao","root","");
+            
+            //query
+            String sql = "select * from employee";
+            statement = connection.createStatement();
+            
+            ResultSet resultSet = statement.executeQuery(sql);
+            
+            while (resultSet.next()) {                
+                Employee std = new Employee(resultSet.getInt("id"), 
+                        resultSet.getString("fullname"), resultSet.getString("gender"), 
+                        resultSet.getString("position"), resultSet.getString("phone_number"), 
+                        resultSet.getInt("age"));
+                employeeList.add(std);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+        
+        return employeeList;
+    }
+    
+    public static void insert(Employee std) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/qlcuahangquanao","root","");
+            
+            //query
+            String sql = "insert into employee(fullname, gender, age, position, phone_number) values(?, ?, ?, ?, ?)";
+            statement = connection.prepareCall(sql);
+            
+            statement.setString(1, std.getFullname());
+            statement.setString(2, std.getGender());
+            statement.setInt(3, std.getAge());
+            statement.setString(4, std.getPosition());
+            statement.setString(5, std.getPhoneNumber());
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static void update(Employee std) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/qlcuahangquanao","root","");
+            
+            //query
+            String sql = "update employee set fullname=?,gender=?,age=?,position=?,phone_number=? where id = ?";
+            statement = connection.prepareCall(sql);
+            
+            statement.setString(1, std.getFullname());
+            statement.setString(2, std.getGender());
+            statement.setInt(3, std.getAge());
+            statement.setString(4, std.getPosition());
+            statement.setString(5, std.getPhoneNumber());
+            statement.setInt(6, std.getId());
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static void delete(int id) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/qlcuahangquanao","root","");
+            
+            //query
+            String sql = "delete from employee where id = ?";
+            statement = connection.prepareCall(sql);
+            
+            statement.setInt(1, id);
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static List<Employee> findByFullname(String fullname) {
+        List<Employee> employeeList = new ArrayList<>();
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/qlcuahangquanao","root","");
+            
+            //query
+            String sql = "select * from employee where fullname like ?";
+            statement = connection.prepareCall(sql);
+            statement.setString(1, "%"+fullname+"%");
+            
+            ResultSet resultSet = statement.executeQuery();
+            
+            while (resultSet.next()) {                
+                Employee std = new Employee(resultSet.getInt("id"), 
+                        resultSet.getString("fullname"), resultSet.getString("gender"), 
+                        resultSet.getString("position"), resultSet.getString("phone_number"), 
+                        resultSet.getInt("age"));
+                employeeList.add(std);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EmployeeModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+        
+        return employeeList;
+    }
+}
